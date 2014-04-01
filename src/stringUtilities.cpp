56087eef49720a27aac1ca8724b0ba623be4933a
Marc Suchard <msuchard@gmail.com>
2014-03-31 19:36:00 -0700

    Some suggestions on standards

--- src/stringUtilities.cpp
+++ src/stringUtilities.cpp
@@ -1,43 +1,60 @@
-#include "stringUtilities.h"
+#ifndef __stringUtilities_cpp__   // All files should have if-guards
+#define __stringUtilities_cpp__
+
+
 
 #include <algorithm>
 #include <cctype>
 #include <functional>
 #include <iterator>
-#include <string>
-#include <vector>
 
+#include "stringUtilities.h" // Include system files first (allows for overriding)
+
+namespace ohdsi {
 namespace stringUtilities {
-std::string &ltrim(std::string &s) {
+
+String& ltrim(String& s) {
 	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 	return s;
 }
 
-std::string &rtrim(std::string &s) {
+String& rtrim(String& s) {
 	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 	return s;
 }
 
-std::string &trim(std::string &s) {
-	return stringUtilities::ltrim(stringUtilities::rtrim(s));
+String& trim(String& s) {
+	return ltrim(rtrim(s)); // Already in namespace
 }
 
-std::string toLowerCase(std::string &s) {
-	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-	return s;
+// This function is a bit strange.  First, you modify s and then you return a copy.  
+// So, both s and the copy are in lower case.
+// I have changed the behavior to first make a copy and then transform, such that s is const
+// std::string toLowerCase(std::string &s) {
+// 	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+// 	return s;
+// }
+
+String toLowerCase(const String& s) {
+	String result;
+	result.resize(s.size());
+	std::transform(s.begin(), s.end(), result.begin(), ::tolower);
+	return result;
 }
 
-std::string removeParentheses(std::string &s) {
+String removeParentheses(const String& s) {
 	if (s.length() > 1 && ((s.at(0) == '\'' && s.at(s.length() - 1) == '\'') || (s.at(0) == '"' && s.at(s.length() - 1) == '"')))
 		return s.substr(1, s.length() - 2);
 	else
 		return s;
 }
 
-std::vector<std::string> multiFindBetween(std::string &source, const std::string pre, const std::string post) {
-	std::vector<std::string> values;
-	std::size_t start = 0;
-	std::size_t end = 0;
+StringVector multiFindBetween(const String& source, const String& pre, const String& post) { // source, pre and post are not modified, so better to pass reference instead of copy
+	typedef std::size_t size_t; // Save typing later, could also do something about std::string::npos
+	
+	StringVector values;
+	size_t start = 0;
+	size_t end = 0;
 	while (start != std::string::npos && end != std::string::npos) {
 		start = source.find(pre, end);
 		if (start != std::string::npos) {
@@ -49,14 +66,19 @@ std::vector<std::string> multiFindBetween(std::string &source, const std::string
 	return values;
 }
 
-std::string replaceAll(std::string source, const std::string search, const std::string replace) {
+// You modify source and then return a copy.  Make copy, then modify
+String replaceAll(const String& source, const String& search, const String& replace) {
+	String result(source);
     size_t pos = 0;
-    while ((pos = source.find(search, pos)) != std::string::npos) {
-         source.replace(pos, search.length(), replace);
+    while ((pos = result.find(search, pos)) != std::string::npos) {
+         result.replace(pos, search.length(), replace);
          pos += replace.length();
     }
-    return source;
+    return result;
 }
 
 
-}
+} // namespace stringUtilities
+} // namespace ohdsi
+
+#endif // __stringUtilities_cpp__
--- src/stringUtilities.h
+++ src/stringUtilities.h
@@ -1,13 +1,27 @@
+#ifndef __stringUtilities_h__   // All files should have if-guards
+#define __stringUtilities_h__
+
 #include <string>
 #include <vector>
 
+namespace ohdsi {
 namespace stringUtilities {
-std::string &ltrim(std::string &s);
-std::string &rtrim(std::string &s);
-std::string &trim(std::string &s);
-std::string toLowerCase(std::string &s);
-std::string removeParentheses(std::string &s);
-std::vector<std::string> multiFindBetween(std::string &source, const std::string pre, const std::string post);
-std::string replaceAll(std::string source, const std::string search, const std::string replace);
-}
-;
+
+	typedef std::string String; // To enable easy code changes if we decide on a different string/vector implementation	
+	typedef std::vector<String> StringVector;
+
+	String& ltrim(String& s); // Do these return references or copies?
+	String& rtrim(String& s);
+	String& trim(String& s);
+// 	String toLowerCase(String& s);
+	String toLowerCase(const String& s);
+// 	String removeParentheses(String& s);
+	String removeParentheses(const String& s);
+// 	StringVector multiFindBetween(String& source, const String pre, const String post); 
+	StringVector multiFindBetween(const String& source, const String& pre, const String& post); // Do you need copies of pre and post?  No, then use reference
+// 	String replaceAll(String source, const String search, const String replace);
+	String replaceAll(const String& source, const String& search, const String& replace);
+}; // namespace stringUtlities
+}; // namespace ohdsi
+
+#endif // __stringUtilities_h__
