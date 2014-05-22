85d8cc7e1b5259968ba86db0a291569715710201
unknown <mschuemi@WPRDUSMJTGLAY.wks.jnj.com>
2014-05-22 03:12:47 -0400

    Added more SQL Server -> Oracle replacement patterns

--- inst/tests/test-translateSql.R
+++ inst/tests/test-translateSql.R
@@ -7,7 +7,7 @@ test_that("translateSQL sql server -> Oracle DATEDIFF", {
 
 
 test_that("translateSQL sql server -> Oracle DATEADD", {
-			sql <- translateSql("SELECT DATEADD(dd,drug_era_end_date,30) FROM drug_era;",sourceDialect = "sql server", targetDialect = "oracle")$sql
+			sql <- translateSql("SELECT DATEADD(dd,30,drug_era_end_date) FROM drug_era;",sourceDialect = "sql server", targetDialect = "oracle")$sql
 			expect_equal(sql, "SELECT (drug_era_end_date + 30) FROM drug_era;")
 		})
 
@@ -20,3 +20,29 @@ test_that("translateSQL sql server -> Oracle DROP TABLE IF EXISTS", {
 			sql <- translateSql("IF OBJECT_ID('cohort', 'U') IS NOT NULL DROP TABLE cohort;",sourceDialect = "sql server", targetDialect = "oracle")$sql
 			expect_equal(sql, "BEGIN\n  EXECUTE IMMEDIATE 'TRUNCATE TABLE  cohort';\n  EXECUTE IMMEDIATE 'DROP TABLE  cohort';\nEXCEPTION\n  WHEN OTHERS THEN\n    IF SQLCODE != -942 THEN\n      RAISE;\n    END IF;\nEND;")
 		})
+
+
+test_that("translateSQL sql server -> Oracle CAST(AS DATE)", {
+  sql <- translateSql("CAST('20000101' AS DATE);",sourceDialect = "sql server", targetDialect = "oracle")$sql
+  expect_equal(sql, "TO_DATE('20000101' , 'yyyymmdd');")
+})
+
+test_that("translateSQL sql server -> Oracle concatenate string operator", {
+  sql <- translateSql("select distinct cast(cast(YEAR(observation_period_start_date) as varchar(4)) +  '01' + '01' as date) as obs_year;",sourceDialect = "sql server", targetDialect = "oracle")$sql
+  expect_equal(sql, "select distinct TO_DATE(TO_CHAR(EXTRACT(YEAR FROM observation_period_start_date) ) || '01' || '01' , 'yyyymmdd') as obs_year;")
+})
+
+test_that("translateSQL sql server -> Oracle RIGHT functions", {
+  sql <- translateSql("select RIGHT(x,4);",sourceDialect = "sql server", targetDialect = "oracle")$sql
+  expect_equal(sql, "select SUBSTR(x,-4);")
+})
+
+test_that("translateSQL sql server -> Oracle complex query", {
+  sql <- translateSql("select CAST(CAST(YEAR(x) AS VARCHAR(12)) + RIGHT('0'+MONTH(x),2) + '01' AS DATE);",sourceDialect = "sql server", targetDialect = "oracle")$sql
+  expect_equal(sql, "select TO_DATE(TO_CHAR(EXTRACT(YEAR FROM x) ) || SUBSTR('0' ||EXTRACT(MONTH FROM x),-2) || '01' , 'yyyymmdd');")
+})
+
+test_that("translateSQL sql server -> Oracle '+' in quote", {
+  sql <- translateSql("select '+';",sourceDialect = "sql server", targetDialect = "oracle")$sql
+  expect_equal(sql, "select '+';")
+})
