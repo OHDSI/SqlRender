8c9f7f8bbd4951e92dabdf6a65be677a8ba9c78c
schuemie <schuemie@ohdsi.org>
2014-06-24 07:02:58 -0400

    Modified Redshift and Postgres translation of dateadd so it now returns a date, not a timestamp. Also added loadRenderTranslateSql function.

--- R/SqlRender.R
+++ R/SqlRender.R
@@ -60,3 +60,39 @@ renderSqlFile <- function(sourceFile, targetFile, ...) {
   sink()
 }
 
+#' Load, render, and translate a SQL file in a package
+#'
+#' @description
+#' \code{loadRenderTranslateSql} Loads a SQL file contained in a package, renders it and translates it to the specified dialect
+#'
+#' @details
+#' This function looks for a SQL file with the specified name in the inst/sql/<dbms> folder of the specified package. 
+#' If it doesn't find it in that folder, it will try and load the file from the inst/sql/sql_server folder and use the
+#' \code{translateSql} function to translate it to the requested dialect. It will subsequently call the \code{renderSql}
+#' function with any of the additional specified parameters.
+#' 
+#' 
+#' @param sqlFilename           The source SQL file
+#' @param packageName           The name of the package that contains the SQL file
+#' @param dbms                  The target dialect. Currently "sql server", "oracle", "postgres", and "redshift" are supported
+#' @param ...                   Parameter values used for \code{renderSql}
+
+#' @examples \dontrun{
+#'   renderedSql <- loadRenderTranslateSql("CohortMethod.sql",packageName = "CohortMethod",dbms = connectionDetails$dbms,CDM_schema = "cdmSchema")
+#' }
+#' @export
+loadRenderTranslateSql <- function(sqlFilename, packageName, dbms="sql server", ...){
+  pathToSql <- system.file(paste("sql/",gsub(" ","_",dbms),sep=""), sqlFilename, package=packageName)
+  mustTranslate <- !file.exists(pathToSql)
+  if (mustTranslate) # If DBMS-specific code does not exists, load SQL Server code and translate after rendering
+    pathToSql <- system.file(paste("sql/","sql_server",sep=""), sqlFilename, package=packageName)      
+  parameterizedSql <- readChar(pathToSql,file.info(pathToSql)$size)  
+  
+  renderedSql <- renderSql(parameterizedSql[1], ...)$sql
+  
+  if (mustTranslate)
+    renderedSql <- translateSql(renderedSql, "sql server", dbms)$sql
+  
+  renderedSql
+}
+
