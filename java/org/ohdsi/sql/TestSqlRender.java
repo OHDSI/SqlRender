2417d3250356179f841335c69462ccd82c1ba8ae
Matthew Young-Lai <myl@google.com>
2017-05-09 13:09:49 -0400

    Remove replacement patterns that lead to infinite loop.  These aren't needed because standard sql can be specified in the jdbc url.

--- java/org/ohdsi/sql/TestSqlRender.java
+++ java/org/ohdsi/sql/TestSqlRender.java
@@ -9,10 +9,12 @@ public class TestSqlRender {
 //		String sql = "SELECT * FROM table {@a = true} ?  {WHERE name = '@name'};";
 //		sql = SqlRender.renderSql(sql, new String[]{"name", "a"}, new String[]{"NA\\joe", "true"});
 //		System.out.println(sql);	
-		String path = "inst/csv/replacementPatterns.csv";
+		//String path = "/Users/myl/mylSqlRender/SqlRender/inst/csv/replacementPatterns.csv";
+		System.out.println(System.getProperty("user.dir"));
+		String path = "/inst/csv/replacementPatterns.csv";
 		String sourceSql = "SELECT TOP 10 * FROM my_table WHERE a = b;";
 		String sql;
-		sql = SqlTranslate.translateSqlWithPath(sourceSql, "postgresql", null, null, path);
+		sql = SqlTranslate.translateSqlWithPath(sourceSql, "bigquery", null, null, path);
 		System.out.println(sql);		
 //		
 //		sql = SqlTranslate.translateSqlWithPath(sourceSql, "oracle", null, null, path);
