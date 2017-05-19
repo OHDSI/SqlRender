b10f3ad7e9f28ccc2a765504ab424165e7dc733b
Matthew Young-Lai <myl@google.com>
2017-05-19 11:06:13 -0400

    Decompose subtraction and extract operators in group by.

--- java/org/ohdsi/sql/TestSqlRender.java
+++ java/org/ohdsi/sql/TestSqlRender.java
@@ -12,55 +12,14 @@ public class TestSqlRender {
 		//String path = "/Users/myl/mylSqlRender/SqlRender/inst/csv/replacementPatterns.csv";
 		String path = "inst/csv/replacementPatterns.csv";
 		//String sourceSql = "SELECT TOP 10 * FROM my_table WHERE a = b;";
-		String sourceSql = "with rawData (person_id, age_value) as\n" +
-				"(\n" +
-				"select p.person_id,\n" +
-				"  MIN(YEAR(observation_period_start_date)) - P.YEAR_OF_BIRTH as age_value\n" +
-				"  from ohdsi.PERSON p\n" +
-				"  JOIN ohdsi.OBSERVATION_PERIOD op on p.person_id = op.person_id\n" +
-				"  group by p.person_id, p.year_of_birth\n" +
-				"),\n" +
-				"overallStats (avg_value, stdev_value, min_value, max_value, total) as\n" +
-				"(\n" +
-				"  select CAST(avg(1.0 * age_value) AS FLOAT) as avg_value,\n" +
-				"  CAST(stdev(age_value) AS FLOAT) as stdev_value,\n" +
-				"  min(age_value) as min_value,\n" +
-				"  max(age_value) as max_value,\n" +
-				"  count_big(*) as total\n" +
-				"  FROM rawData\n" +
-				"),\n" +
-				"ageStats (age_value, total, rn) as\n" +
-				"(\n" +
-				"  select age_value, count_big(*) as total, row_number() over (order by age_value) as rn\n" +
-				"  from rawData\n" +
-				"  group by age_value\n" +
-				"),\n" +
-				"ageStatsPrior (age_value, total, accumulated) as\n" +
-				"(\n" +
-				"  select s.age_value, s.total, sum(p.total) as accumulated\n" +
-				"  from ageStats s\n" +
-				"  join ageStats p on p.rn <= s.rn\n" +
-				"  group by s.age_value, s.total, s.rn\n" +
-				")\n" +
-				"select 103 as analysis_id,\n" +
-				"  o.total as count_value,\n" +
-				"        o.min_value,\n" +
-				"        o.max_value,\n" +
-				"        o.avg_value,\n" +
-				"        o.stdev_value,\n" +
-				"        MIN(case when p.accumulated >= .50 * o.total then age_value end) as median_value,\n" +
-				"        MIN(case when p.accumulated >= .10 * o.total then age_value end) as p10_value,\n" +
-				"        MIN(case when p.accumulated >= .25 * o.total then age_value end) as p25_value,\n" +
-				"        MIN(case when p.accumulated >= .75 * o.total then age_value end) as p75_value,\n" +
-				"        MIN(case when p.accumulated >= .90 * o.total then age_value end) as p90_value\n" +
-				"INTO #tempResults\n" +
-				"from ageStatsPrior p\n" +
-				"CROSS JOIN overallStats o\n" +
-				"GROUP BY o.total, o.min_value, o.max_value, o.avg_value, o.stdev_value\n" +
-				";";
+		String sourceSql = "select 102 as analysis_id,  CAST(p1.gender_concept_id AS VARCHAR(255)) as stratum_1, CAST(year(op1.index_date) - p1.YEAR_OF_BIRTH AS VARCHAR(255)) as stratum_2, COUNT_BIG(p1.person_id) as count_value\n" +
+				"from ohdsi.PERSON p1\n" +
+				"        inner join (select person_id, MIN(observation_period_start_date) as index_date from ohdsi.OBSERVATION_PERIOD group by PERSON_ID) op1\n" +
+				"        on p1.PERSON_ID = op1.PERSON_ID\n" +
+				"group by p1.gender_concept_id, year(op1.index_date) - p1.YEAR_OF_BIRTH;";
 		String sql;
 		sql = SqlTranslate.translateSqlWithPath(sourceSql, "bigquery", null, null, path);
-		System.out.println(sql);		
+		System.out.println(sql.toLowerCase());
 //		
 //		sql = SqlTranslate.translateSqlWithPath(sourceSql, "oracle", null, null, path);
 //		System.out.println(sql);
