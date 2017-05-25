174a38dcbb45e820fa50f15d55efa3aef1601bd6
Matthew Young-Lai <myl@google.com>
2017-05-25 15:03:07 -0400

    More reverts.

--- java/org/ohdsi/sql/TestSqlRender.java
+++ java/org/ohdsi/sql/TestSqlRender.java
@@ -9,17 +9,11 @@ public class TestSqlRender {
 //		String sql = "SELECT * FROM table {@a = true} ?  {WHERE name = '@name'};";
 //		sql = SqlRender.renderSql(sql, new String[]{"name", "a"}, new String[]{"NA\\joe", "true"});
 //		System.out.println(sql);	
-		//String path = "/Users/myl/mylSqlRender/SqlRender/inst/csv/replacementPatterns.csv";
 		String path = "inst/csv/replacementPatterns.csv";
-		//String sourceSql = "SELECT TOP 10 * FROM my_table WHERE a = b;";
-		String sourceSql = "select 101 as analysis_id,   CAST(year(op1.index_date) - p1.YEAR_OF_BIRTH AS VARCHAR(255)) as stratum_1, COUNT_BIG(p1.person_id) as count_value\n" +
-				"from ohdsi.PERSON p1\n" +
-				"        inner join (select person_id, MIN(observation_period_start_date) as index_date from ohdsi.OBSERVATION_PERIOD group by PERSON_ID) op1\n" +
-				"        on p1.PERSON_ID = op1.PERSON_ID\n" +
-				"group by year(op1.index_date) - p1.YEAR_OF_BIRTH;";
+		String sourceSql = "SELECT TOP 10 * FROM my_table WHERE a = b;";
 		String sql;
-		sql = SqlTranslate.translateSqlWithPath(sourceSql, "bigquery", null, null, path);
-		System.out.println(sql.toLowerCase());
+		sql = SqlTranslate.translateSqlWithPath(sourceSql, "postgresql", null, null, path);
+		System.out.println(sql);		
 //		
 //		sql = SqlTranslate.translateSqlWithPath(sourceSql, "oracle", null, null, path);
 //		System.out.println(sql);
