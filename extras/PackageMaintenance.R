0206ab141cf7194bb7e073034cfecbdc108d214c
schuemie <schuemie@ohdsi.org>
2020-03-16 12:36:34 -0400

    Increasing version number to 1.6.4. Regenerating documentation

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -1,6 +1,6 @@
 # @file PackageMaintenance
 #
-# Copyright 2019 Observational Health Data Sciences and Informatics
+# Copyright 2020 Observational Health Data Sciences and Informatics
 #
 # This file is part of SqlRender
 # 
@@ -20,9 +20,10 @@
 OhdsiRTools::formatRFolder()
 OhdsiRTools::checkUsagePackage("SqlRender")
 OhdsiRTools::updateCopyrightYearFolder()
+devtools::spell_check()
 
 # Create manual and vignettes:
-shell("rm extras/SqlRender.pdf")
+unlink("extras/SqlRender.pdf")
 shell("R CMD Rd2pdf ./ --output=extras/SqlRender.pdf")
 
 rmarkdown::render("vignettes/UsingSqlRender.Rmd",
