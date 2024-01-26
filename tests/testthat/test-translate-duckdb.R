98d760dc9b06eced2f3e4f4f12be756e401cc739
egillax <egillax@gmail.com>
2024-01-26 11:48:44 +0100

    fix targetDialect in added test

--- tests/testthat/test-translate-duckdb.R
+++ tests/testthat/test-translate-duckdb.R
@@ -228,6 +228,6 @@ test_that("translate sql server -> DuckDB add days with period", {
   )
 })
 test_that("translate sql server -> duckdb NEWID", {
-  sql <- translate("SELECT NEWID()", targetDialect = "redshift")
+  sql <- translate("SELECT NEWID()", targetDialect = "duckdb")
   expect_equal_ignore_spaces(sql, "SELECT uuid()") 
 })
